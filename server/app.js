22148226ffb82dc5454fede89b995c4fd70f9036
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-05 23:56:48 +0100

    Configure remote DB connection

--- server/app.js
+++ server/app.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const userRouter = require("./src/routes/userRoutes");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DB_PASSWORD);
+mongoose
+  .connect(
+    DB
+    //     , {
+    //     useNewUrlParser: true,
+    //     useCreateIndex: true,
+    //     useFindAndModify: true,
+    //   }
+  )
+  .then((con) => {
+    // console.log(con.connections);
+    console.log("DB connection successful!");
+  });
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use("/api/v1/users", userRouter);
 
 app.listen(port, () => {
