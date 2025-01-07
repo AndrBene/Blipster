48f65874a7654fed7456bf9dce7d095b55bc6137
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-07 11:27:19 +0100

    Refactor Comment routes

--- server/src/routes/commentRoutes.js
+++ server/src/routes/commentRoutes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const commentRouter = express.Router();
 const commentController = require("../controllers/commentController");
 
-commentRouter.route("/:postId").get(commentController.getComments);
-commentRouter.route("/:postId").post(commentController.addComment);
+commentRouter
+  .route("/:postId")
+  .get(commentController.getComments)
+  .post(commentController.addComment);
+
 commentRouter.route("/:commentId").delete(commentController.deleteComment);
 
 module.exports = commentRouter;
