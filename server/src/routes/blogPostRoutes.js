c4250c4db955cf2529b2099676f65fc8ef8a2071
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-07 11:26:44 +0100

    Refactor BlogPost routes

--- server/src/routes/blogPostRoutes.js
+++ server/src/routes/blogPostRoutes.js
@@ -2,14 +2,15 @@ const express = require("express");
 const blogPostRouter = express.Router();
 const blogPostController = require("../controllers/blogPostController");
 
-blogPostRouter.route("/").get(blogPostController.getAllBlogPosts);
+blogPostRouter
+  .route("/")
+  .get(blogPostController.getAllBlogPosts)
+  .post(blogPostController.createNewBlogPost);
 
-blogPostRouter.route("/").post(blogPostController.createNewBlogPost);
-
-blogPostRouter.route("/:id").get(blogPostController.getBlogPost);
-
-blogPostRouter.route("/:id").put(blogPostController.updateBlogPost);
-
-blogPostRouter.route("/:id").delete(blogPostController.deleteBlogPost);
+blogPostRouter
+  .route("/:id")
+  .get(blogPostController.getBlogPost)
+  .put(blogPostController.updateBlogPost)
+  .delete(blogPostController.deleteBlogPost);
 
 module.exports = blogPostRouter;
