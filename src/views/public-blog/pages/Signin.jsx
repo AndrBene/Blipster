9ce84823ff9a77177c30171970b2f7bc69fe78b4
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-18 19:21:02 +0100

    Added Login page view

--- src/views/public-blog/pages/Signin.jsx
+++ src/views/public-blog/pages/Signin.jsx
@@ -1,7 +1,52 @@
+import { Form, Link } from 'react-router-dom';
+
 function Signin() {
   return (
-    <div className="mt-10 flex flex-col justify-start text-black">
-      Signin
+    <div className="mx-80 mt-10 text-black">
+      <div className="mb-16 text-3xl font-bold">Login</div>
+      {/* <Form method="POST"> */}
+      <form>
+        <div className="mb-5 flex items-center gap-4 text-lg">
+          <label htmlFor="" className="basis-20 text-xl">
+            Username
+          </label>
+          <div className="grow text-lg">
+            <input
+              placeholder="username"
+              className="input"
+              type="text"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-5 flex items-center gap-4 text-lg">
+          <label htmlFor="" className="basis-20 text-xl">
+            Password
+          </label>
+          <div className="grow text-lg">
+            <input
+              type="text"
+              required
+              className="input"
+              placeholder="password"
+            />
+          </div>
+        </div>
+
+        <div className="mt-10 flex flex-col items-center">
+          <button className="mb-2 rounded-full bg-slate-800 px-12 py-3 text-xl uppercase text-white transition-colors duration-200 hover:bg-slate-700 focus:bg-slate-700 focus:outline-none">
+            Login
+          </button>
+          <div className="text-lg">
+            Or{' '}
+            <Link to="/register" className="underline">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </form>
+      {/* </Form> */}
     </div>
   );
 }
