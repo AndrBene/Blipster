f432e270acd4c6700215a254c0dd6bdf1df9381f
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-18 19:25:16 +0100

    Added Register page view

--- src/views/public-blog/pages/Register.jsx
+++ src/views/public-blog/pages/Register.jsx
@@ -1,7 +1,72 @@
 function Register() {
   return (
-    <div className="mt-10 flex flex-col justify-start text-black">
-      Register
+    <div className="mx-80 mt-10 text-black">
+      <div className="mb-16 text-3xl font-bold">Register</div>
+      {/* <Form method="POST"> */}
+      <form>
+        <div className="mb-5 flex items-center gap-4 text-lg">
+          <label htmlFor="" className="basis-40 text-xl">
+            Email
+          </label>
+          <div className="grow text-lg">
+            <input
+              placeholder="email"
+              className="input"
+              type="text"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-5 flex items-center gap-4 text-lg">
+          <label htmlFor="" className="basis-40 text-xl">
+            Username
+          </label>
+          <div className="grow text-lg">
+            <input
+              placeholder="username"
+              className="input"
+              type="text"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-5 flex items-center gap-4 text-lg">
+          <label htmlFor="" className="basis-40 text-xl">
+            Password
+          </label>
+          <div className="grow text-lg">
+            <input
+              type="text"
+              required
+              className="input"
+              placeholder="password"
+            />
+          </div>
+        </div>
+
+        <div className="mb-5 flex items-center gap-4 text-lg">
+          <label htmlFor="" className="basis-40 text-xl">
+            Confirm password
+          </label>
+          <div className="grow text-lg">
+            <input
+              type="text"
+              required
+              className="input"
+              placeholder="confirm password"
+            />
+          </div>
+        </div>
+
+        <div className="mt-10 flex flex-col items-center">
+          <button className="mb-2 rounded-full bg-slate-800 px-12 py-3 text-xl uppercase text-white transition-colors duration-200 hover:bg-slate-700 focus:bg-slate-700 focus:outline-none">
+            Register
+          </button>
+        </div>
+      </form>
+      {/* </Form> */}
     </div>
   );
 }
