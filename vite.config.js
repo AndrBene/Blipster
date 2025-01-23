2390e58c50dfc9f32407f5144c791f8edf931395
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-23 14:26:55 +0100

    Serve static files for dev server

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: 'src/client/public-blog', // Set root to vite-dev-server
+  publicDir: '../../../public', // serve plain static assets w.r.t. root folder
   plugins: [react()],
 });
