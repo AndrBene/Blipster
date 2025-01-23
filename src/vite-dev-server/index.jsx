0d5d499ebe151daeddcd7bfa8b9f4704b3ef1d95
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-23 15:40:45 +0100

    Created dev server folder for vite

--- src/vite-dev-server/index.jsx
+++ src/vite-dev-server/index.jsx
@@ -1,8 +1,8 @@
-import AppLayout from '../../views/public-blog/AppLayout.jsx';
+import AppLayout from '../views/public-blog/AppLayout.jsx';
 import ReactDOM from 'react-dom/client';
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import '../../style/input.css';
+import '../style/input.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src/client/public-blog', // Set root to vite-dev-server
-  publicDir: '../../../public', // serve plain static assets w.r.t. root folder
+  root: 'src/vite-dev-server', // Set root to vite-dev-server
+  publicDir: '../../public', // serve plain static assets w.r.t. root folder
   plugins: [react()],
 });
