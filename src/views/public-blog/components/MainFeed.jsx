adf967748b213915c3d7848fac081c95a49d6931
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-27 11:33:33 +0100

    Fetch posts from DB

--- src/views/public-blog/components/MainFeed.jsx
+++ src/views/public-blog/components/MainFeed.jsx
@@ -1,56 +1,17 @@
+import { useEffect, useState } from 'react';
 import Post from './Post';
 
-const feed = [
-  {
-    id: '1',
-    title: 'Wildfires',
-    text: `Thread of heartbreaking moments just before disaster strikes
-(Don't open it if you have a soft heart)`,
-    topic: 'nature',
-    image: 'wildfires.jpg',
-    views: 39,
-    comments: 22,
-    date: '10 Oct, 2024',
-  },
-  {
-    id: '2',
-    title: 'Biden news',
-    text: `Four years ago, in the middle of a pandemic, we needed a leader with the character to put politics aside and do what was right. That’s what 
-@JoeBiden
- did.
- 
-At a time when our economy was reeling, he drove what would become the world’s strongest recovery – with 17 million new jobs, historic wage gains, and lower health care costs. He passed landmark legislation to rebuild our nation’s infrastructure and address the threat of climate change. 
- 
-I’m grateful to Joe for his leadership, his friendship, and his lifetime of service to this country we love.`,
-    topic: 'politics',
-    image: 'biden.jpg',
-    views: 15,
-    comments: 3,
-    date: '12 Oct, 2024',
-  },
-  {
-    id: '3',
-    title: 'Toughest bodyguard around',
-    text: `10 things you don’t know about Messi bodyguard`,
-    topic: 'sports',
-    image: 'messibodiguard.webp',
-    views: 83,
-    comments: 40,
-    date: '10 Oct, 2024',
-  },
-  {
-    id: '4',
-    title: 'STEPH CURRY. INEVITABLE 😤',
-    text: `His seventh three of the game 💪`,
-    topic: 'sports',
-    image: 'stephcurry.jpg',
-    views: 120,
-    comments: 13,
-    date: '10 Oct, 2024',
-  },
-];
-
 function MainFeed() {
+  const [feed, setFeed] = useState([]);
+
+  useEffect(function () {
+    fetch('http://localhost:3000/api/v1/posts')
+      .then((res) => res.json())
+      .then((json) => {
+        setFeed(json.data.blogPosts);
+      });
+  }, []);
+
   return (
     <div>
       <div className="mt-2 border-2 border-white">
