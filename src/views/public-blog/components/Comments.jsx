f67588dddaef9e8b5db98de6843987d1ffbcf08a
Andrea Benedetto <andri.ben09@gmail.com>
2025-01-28 14:39:45 +0100

    Fix comment section when no comments are present

--- src/views/public-blog/components/Comments.jsx
+++ src/views/public-blog/components/Comments.jsx
@@ -3,9 +3,11 @@ import Comment from './Comment';
 function Comments({ comments }) {
   return (
     <div className="mb-20">
-      <div className="mb-6 flex flex-col justify-start text-2xl font-medium text-black">
-        Comments
-      </div>
+      {comments?.length > 0 ? (
+        <div className="mb-6 flex flex-col justify-start text-2xl font-medium text-black">
+          Comments
+        </div>
+      ) : null}
       {comments?.map((comment) => {
         return <Comment key={comment._id} comment={comment} />;
       })}
