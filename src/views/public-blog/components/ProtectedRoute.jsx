d45cde82294fb7c66e9cc9cfd12b2e9d269887f5
Andrea Benedetto <andri.ben09@gmail.com>
2025-02-24 23:57:37 +0100

    Fix auth bug in ProtectedRoute

--- src/views/public-blog/components/ProtectedRoute.jsx
+++ src/views/public-blog/components/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate } from 'react-router-dom';
 import { fetchUserIsAuthenticated } from '../services/authApi';
 import { useQuery } from '@tanstack/react-query';
+import Loader from './Loader';
 
 function ProtectedRoute({ children }) {
   console.log('ProtectedRoute');
-  const { data: isAuthenticated } = useQuery({
+  const { isLoading, data: isAuthenticated } = useQuery({
     queryKey: ['isAuthenticated'],
     queryFn: fetchUserIsAuthenticated,
     meta: {
@@ -14,6 +15,10 @@ function ProtectedRoute({ children }) {
   });
 
   console.log('isAuthenticated:', isAuthenticated);
+
+  if (isLoading) {
+    return <Loader text={''} />; // Show loading until the query resolves
+  }
 
   return isAuthenticated ? (
     children
