592b6d2d3c28b6e245373a5cd19637afd3572e27
Andrea Benedetto <andri.ben09@gmail.com>
2025-03-12 16:12:22 +0100

    Fix home selection in top bar when reloading about page

--- src/views/public-blog/components/TopBar.jsx
+++ src/views/public-blog/components/TopBar.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   HiOutlineHome,
   HiOutlineInformationCircle,
 } from 'react-icons/hi2';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function TopBar() {
-  const [isHome, setIsHome] = useState(true);
+  const location = useLocation();
+  const [isHome, setIsHome] = useState(null);
+
   const selectedStyle =
     'border-slate-800 border-b-[1px] text-slate-800 dark:text-white dark:stroke-white dark:border-white';
   const unselectedStyle = 'text-slate-500';
 
+  useEffect(
+    function () {
+      if (location.pathname === '/home') {
+        setIsHome(true);
+      } else {
+        setIsHome(false);
+      }
+    },
+    [location.pathname],
+  );
+
   return (
-    <div className="sticky top-0 flex content-start gap-x-5 border-b-[1px] border-gray-200 bg-white text-base dark:border-slate-500 dark:bg-slate-950 md:text-lg xl:text-xl">
+    <div className="sticky top-0 flex content-start gap-x-5 border-b-[1px] border-gray-200 bg-white text-base md:text-lg xl:text-xl dark:border-slate-500 dark:bg-slate-950">
       <Link
-        to="/"
+        to={`/home?page=${localStorage.getItem('currentPageNum') || 1}`}
         className={`flex cursor-pointer items-center gap-2 ${isHome ? selectedStyle : unselectedStyle} p-5`}
         onClick={() => {
           setIsHome(true);
