786d82dfa1c47ddac14c8a6c116fc43dfc2424b9
Andrea Benedetto <andri.ben09@gmail.com>
2025-03-27 16:52:48 +0100

    Add service for communication with server API

--- src/views/admin/app/app.config.ts
+++ src/views/admin/app/app.config.ts
@@ -5,11 +5,13 @@ import {
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideCharts(withDefaultRegisterables()),
+    provideHttpClient(),
   ],
 };
