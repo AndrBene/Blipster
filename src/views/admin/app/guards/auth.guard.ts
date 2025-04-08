343968649b2739d494bd25757c3746c197a91d70
Andrea Benedetto <andri.ben09@gmail.com>
2025-04-08 14:56:35 +0200

    Fix auth guard

--- src/views/admin/app/guards/auth.guard.ts
+++ src/views/admin/app/guards/auth.guard.ts
@@ -14,16 +14,13 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
 
   const obsIsFetching = toObservable(authService.query.isFetching);
-  const obsData = toObservable(authService.query.data);
 
   return obsIsFetching.pipe(
     filter((isFetching) => !isFetching), // Wait for fetch to complete
-    switchMap(() => obsData), // Wait for the user auth data
-    map((data) => {
-      // map user auth data to boolean value
+    map(() => {
       if (authService.query.isError()) {
         return new RedirectCommand(router.parseUrl('/signin'));
-      } else if (data?.authenticated) {
+      } else if (authService.query.data()?.authenticated) {
         return true;
       } else {
         return new RedirectCommand(router.parseUrl('/signin'));
@@ -37,15 +34,12 @@ export const authGuardAlreadyAuth: CanActivateFn = (route, state) => {
   const router = inject(Router);
 
   const obsIsFetching = toObservable(authService.query.isFetching);
-  const obsData = toObservable(authService.query.data);
 
   return obsIsFetching.pipe(
     filter((isFetching) => !isFetching), // Wait for fetch to complete
-    switchMap(() => obsData), // Wait for the user auth data
-    filter((data) => data !== undefined),
-    map((data) => {
+    map(() => {
       // map user auth data to boolean value
-      if (!data?.authenticated) {
+      if (!authService.query.data()?.authenticated) {
         return true;
       } else {
         return new RedirectCommand(router.parseUrl('/'));
